2d834910110e4ab876582ce6dfea10cae361a45d
Mariel Hoepelman <marielhoepelman@gmail.com>
2017-12-28 20:43:46 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,5 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
+def using_until(levitation_force)
+    until levitation_force == 10
+      puts "Wingardium Leviosa"
+    end      #your code here
 end
-
